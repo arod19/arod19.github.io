92a78633867042dc3a123ad627b8c6cfbeab889a
Angel <angelrodriguezleiva@gmail.com>
2023-07-22 12:54:44 -0400

    box

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function detectiOSARQuickLook() {
   const testAnchor = document.createElement("a");
   return testAnchor.relList.supports("ar");
 }
+
+const modelViewer = document.querySelector("model-viewer");
+     
+window.switchSrc = (element, name) => {
+    const base = "/" + name;
+    modelViewer.src = base + '.glb';
+    modelViewer.poster = base + '.webp';
+    const slides = document.querySelectorAll(".slide");
+    slides.forEach((element) => {
+        element.classList.remove("selected");
+    });
+    element.classList.add("selected");
+}
+
+document.querySelector(".slider").addEventListener('beforexrselect', (ev) => {
+    ev.preventDefault();
+});
